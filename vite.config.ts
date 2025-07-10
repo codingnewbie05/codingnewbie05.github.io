ae7702baa04113c6b20246c486c8ecaad01ea157
codingnewbie05 <codingnewbie05@gmail.com>
2025-07-10 23:07:38 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/newbie-web-haven/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/codingnewbie05.github.io/' : '/',
   server: {
     host: "::",
     port: 8080,
